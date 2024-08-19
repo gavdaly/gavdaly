ba42db2e97c799979eb249dc0efc378ac0588618
Gavin Daly <gav.d@me.com>
2024-08-19 13:04:04 -0400

    refactor: Integrate ProjectListing component for selected projects in index page

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,14 +2,23 @@ export type Project = {
   title: string;
   techs: string[];
   link: string;
+  description?: string;
   isComingSoon?: boolean;
 };
 
 const projects: Project[] = [
   {
     title: "Clock In",
-    techs: ["Leptos", "Axum", "Postgres", "Cloudflare Tunnel, Docker", "OpenProps"],
-    link: "https://github.com/gavdaly/dictionary-app",
+    techs: [
+      "Leptos",
+      "Axum",
+      "Postgres",
+      "Cloudflare Tunnel",
+      "Docker",
+      "OpenProps",
+    ],
+    link: "https://github.com/gavdaly/clockr",
+    description: "A single tenent timetracking app.",
   },
   {
     title: "GavDev",
@@ -19,7 +28,7 @@ const projects: Project[] = [
   {
     title: "Lordly",
     techs: ["HTML", "CSS"],
-    link: "https://github.com/gavdaly",
+    link: "https://github.com/lordly",
     isComingSoon: true,
   },
 ];

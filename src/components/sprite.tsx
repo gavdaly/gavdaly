3e3354be8274641f6b4e3c468889a1a2dea8d116
Gavin Daly <gav.d@me.com>
2024-12-27 19:06:24 -0500

    fix: load the svg's into a svg element rather then a div
    - change the `div` element to `svg` and add xmlns attribute
    - `sprite.svg` remove the `svg` element

--- src/components/sprite.tsx
+++ src/components/sprite.tsx
@@ -1,5 +1,5 @@
 import sprite from "./sprite.svg?raw";
 
 export function Sprite() {
-  return <div innerHTML={sprite} />;
+  return <svg xmlns="http://www.w3.org/2000/svg" innerHTML={sprite} />;
 }

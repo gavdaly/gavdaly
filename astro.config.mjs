6688616aa015d37fba6ab16baefd7bb27ad14dfa
Gavin Daly <gavin@gavdaly.com>
2026-08-26 13:10:59 -0400

    chore: update dependencies and fix Cloudflare deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import mdx from "@astrojs/mdx";
@@ -8,7 +8,10 @@ import { SITE_URL } from "./src/data/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt(), solidJs(), mdx()],
+  integrations: [sitemap(), robotsTxt(), solidJs(), mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   trailingSlash: "never",
   site: SITE_URL,
 
